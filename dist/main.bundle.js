eba1834ba106892c9b9fb3c9a4576e57c55cadaf
Hisham <159368595+IAmMyGuy21th@users.noreply.github.com>
2024-09-10 16:39:59 -0700

    Update main.bundle.js e*5

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -17265,7 +17265,7 @@
 					down: s,
 					left: a
 				}), $p(this, zp, "f").increment()), n && !$p(this, Op, "f") && $p(this, Fp, "f")) {
-				const e = 4e3;
+				const e = 5 * 4e3;
 				$p(this, Pp, "f").applyEngineForce(e, 10), $p(this, Pp, "f").applyEngineForce(e, 15)
 			} else $p(this, Pp, "f").applyEngineForce(0, 10), $p(this, Pp, "f").applyEngineForce(0, 15);
 			if (s && !$p(this, Op, "f") && $p(this, Fp, "f"))
